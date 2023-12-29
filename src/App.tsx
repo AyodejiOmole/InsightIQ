eadd64553c3937aef2c04e0cf00fa0a908dde750
AyodejiOmole <omoledaniel13@gmail.com>
2023-12-29 17:41:58 +0100

    added hero and demo to app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react';
 import Demo from './components/Demo';
 import Hero from './components/Hero';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <main>
-      <div className="main">
-        
-      </div>
-      
+      <Hero/>
+      <Demo/>
     </main>
   )
 }
