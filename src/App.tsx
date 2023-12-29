90f69694ec97cd42cb2f52a5878c37e4aea2f9e0
AyodejiOmole <omoledaniel13@gmail.com>
2023-12-29 17:33:05 +0100

    project setup ready

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Demo from './components/Demo';
+import Hero from './components/Hero';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <div className="main">
+        
+      </div>
+      
+    </main>
+  )
+}
+
+export default App
